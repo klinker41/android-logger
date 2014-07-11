727a8b3eca20406d00939d951beffcc6862306ff
Jake Klinker <jklinker1@gmail.com>
2014-07-11 09:02:10 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# Android-Logger
+
+This is a simple library that will allow Android apps to log both to the internal log, as well
+as an external file so that users can easily grab logs for the application if you need one from
+them.
+
+Usage is simple, just switch your imports from:
+
+```
+import android.util.Log;
+```
+
+to 
+
+```
+import com.klinker.android.Log;
+```
+
+For any of the logging to take place, you need to call Log.setDebug(true). I would not suggest
+doing this for all users because logging takes time! Either only enable it for your debugging
+and then disable it for production versions, or add a preference to your app for users to be
+able to log everything and then tell them where the log is located so that they can send it to
+you.
+
+You can set the path of your log as well with
+
+```
+Log.setPath("EvolveSMS/log.txt");
+```
+
+Please note, to use this you will need the following permission in your AndroidManifest.xml
+
+```
+<uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE"/>
+```
+
+And one more thing, the log will be wiped out and rewritten when it gets over 2 MB.
+
+## License
+
+    Copyright 2013-2014 Jacob Klinker
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
---
+++ src/com/klnker/android/logger/Log.java
@@ -0,0 +1,152 @@
+/*
+ * Copyright 2014 Jacob Klinker
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.klnker.android.logger;
+
+import android.os.Environment;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class Log {
+
+    private static final String TAG = "Log";
+
+    private static boolean DEBUG = false;
+    private static String PATH = "ApplicationLog.txt";
+
+    public static void setDebug(boolean debug) {
+        DEBUG = debug;
+    }
+
+    public static void setPath(String path) {
+        if (path.endsWith("/")) {
+            PATH = path + "ApplicationLog.txt";
+        } else if (!path.endsWith(".txt")) {
+            PATH  = path + ".txt";
+        } else {
+            PATH = path;
+        }
+    }
+
+    public static void e(String tag, String message) {
+        if (DEBUG) {
+            int logResult = android.util.Log.e(tag, message);
+            if (logResult > 0)
+                logToFile(tag, message);
+        }
+    }
+
+    public static void e(String tag, String message, Throwable error) {
+        if (DEBUG) {
+            int logResult = android.util.Log.e(tag, message, error);
+            if (logResult > 0)
+                logToFile(tag, message + "\r\n" + android.util.Log.getStackTraceString(error));
+        }
+    }
+
+    public static void v(String tag, String message) {
+        if (DEBUG) {
+            int logResult = android.util.Log.v(tag, message);
+            if (logResult > 0)
+                logToFile(tag, message);
+        }
+    }
+
+    public static void v(String tag, String message, Throwable error) {
+        if (DEBUG) {
+            int logResult = android.util.Log.v(tag, message, error);
+            if (logResult > 0)
+                logToFile(tag, message + "\r\n" + android.util.Log.getStackTraceString(error));
+        }
+    }
+
+    public static void d(String tag, String message) {
+        if (DEBUG) {
+            int logResult = android.util.Log.d(tag, message);
+            if (logResult > 0)
+                logToFile(tag, message);
+        }
+    }
+
+    public static void d(String tag, String message, Throwable error) {
+        if (DEBUG) {
+            int logResult = android.util.Log.d(tag, message, error);
+            if (logResult > 0)
+                logToFile(tag, message + "\r\n" + android.util.Log.getStackTraceString(error));
+        }
+    }
+
+    public static void i(String tag, String message) {
+        if (DEBUG) {
+            int logResult = android.util.Log.i(tag, message);
+            if (logResult > 0)
+                logToFile(tag, message);
+        }
+    }
+
+    public static void i(String tag, String message, Throwable error) {
+        if (DEBUG) {
+            int logResult = android.util.Log.i(tag, message, error);
+            if (logResult > 0)
+                logToFile(tag, message + "\r\n" + android.util.Log.getStackTraceString(error));
+        }
+    }
+
+    public static void w(String tag, String message) {
+        if (DEBUG) {
+            int logResult = android.util.Log.w(tag, message);
+            if (logResult > 0)
+                logToFile(tag, message);
+        }
+    }
+
+    public static void w(String tag, String message, Throwable error) {
+        if (DEBUG) {
+            int logResult = android.util.Log.w(tag, message, error);
+            if (logResult > 0)
+                logToFile(tag, message + "\r\n" + android.util.Log.getStackTraceString(error));
+        }
+    }
+
+    private static String getDateTimeStamp() {
+        Date dateNow = Calendar.getInstance().getTime();
+        return (DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT, Locale.US).format(dateNow));
+    }
+
+    private static void logToFile(String tag, String message) {
+        try {
+            File logFile = new File(Environment.getExternalStorageDirectory(), PATH);
+            if (!logFile.exists())
+                logFile.createNewFile();
+            if (logFile.length > 2097152) { // 2 MB
+                logFile.delete();
+                logFile.createNewFile();
+            }
+            BufferedWriter writer = new BufferedWriter(new FileWriter(logFile, true));
+            writer.write(String.format("%1s [%2s]:%3s\r\n", getDateTimeStamp(), tag, message));
+            writer.close();
+        } catch (IOException e) {
+            android.util.Log.e(TAG, "Unable to log exception to file.");
+        }
+    }
+}
